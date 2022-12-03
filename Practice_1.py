45f143d842a11fccda1de5f4a959576f6c7e80a7
Sachin Kumar Kunwar <114437201+SachinKumarKunwar@users.noreply.github.com>
2022-12-04 00:14:53 +0530

    Add files via upload

--- Practice_1.py
+++ Practice_1.py
@@ -1,21 +1,5 @@
-#take user input (string)
-#if the len of string is a greater than 3 
-#add "ing" as a suffix in the original string
-#else print the same string given by user
-
-from platformdirs import user_cache_dir
-
-
-user =  input("Give me word")
-
-
-length = len(user)
-
-
-def main():
-    if length> 3:
-        print(user+"ing")
-    else:
-        print(user)
-
-main()
+email = input("Enter Your Email: ").strip()
+username = email[:email.index("@")]
+domain_name = email[email.index("@")+1:]
+print("The Username is:"+username)
+print("The Domain is:"+domain_name.upper())
